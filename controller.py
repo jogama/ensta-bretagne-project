0408ea73af234d1616de244519bbe15d1136e451
Jonathan Garcia-Mallen <jogama@mit.edu>
2017-06-14 10:31:45 +0200

    fixed potential field islands. level curve controller runs, doesn't work.

--- controller.py
+++ controller.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-from potential_field_islands import draw_field
+from potential_field_islands import draw_field, make_islands
 import matplotlib.pyplot as plt 
 import roblib as rl
 import numpy as np
@@ -9,7 +9,7 @@ import numpy as np
 There is not a single convention as to the definition of the state in robmoocpy code. Here, it will be a numpy array x = [x, y, θ]. This would more elegantly bre represented as a vector with respect to the origin. However, we will first implement this in a "yo, it just works this way" kind of way. Aiming for easiest correct implementation, even if it is somewhat cumbersome. 
 """
 
-def follow_level_curve(height, gradient):
+def follow_level_curve(height, desired_height, gradient):
     '''
     Simple proportional controller to follow a level
     Args:
@@ -19,21 +19,20 @@ def follow_level_curve(height, gradient):
     Returns:
       xdot = (x, y) 
     '''
-    # xdot = v = r - g * (V(x) - Vₒ)
-    # return xdot
-    
-    pass
+    ω = desired_height - height
+    return ω
+
 
 def straight_line(x):
     ''' toy controller to go in a straight line. For some reason, it also accelerates.'''
     x = x.flatten()
     v = x[2]
     θ = np.pi / 2
-    x0 = x[0] + v * np.cos(θ)
-    x1 = x[1] + v * np.sin(θ)
-    return np.array([[x0, x1, v, 0]]).T
+    x0 = x[0]  + v * np.cos(θ)
+    x1 = x[1]  + v * np.sin(θ)
+    return np.array([[x0, x1, v, θ]]).T
     
-def f(x,u):
+def f(x,u=np.array([[0],  [0.3]])):
     ''' this seems to be the defualt controller.'''
     
     x,u  = x.flatten(), u.flatten()
@@ -47,7 +46,26 @@ def run_car(duration, dt=.1):
     fig  = plt.figure(0)
     ax   = fig.add_subplot(111, aspect='equal')
     xmin,xmax,ymin,ymax=-5,5,-5,5
+    V_0   = 0.25 # desired height, or potential
 
+    # make a controller that includes the gradient
+    # the curve following controller does does not localize, but uses the 
+    Mx, My, VX, VY, V = make_islands(xmin, xmax, ymin, ymax)
+    
+    flc = lambda x: np.array([x[0], x[1], x[2],
+                              follow_level_curve(V[x[0], x[1]],
+                                                 V_0,
+                                                 np.array([VX[x[0], x[1]], VY[x[0], x[1]]]))])
+    def flc(x):
+        # make a controller that includes the gradient
+        # the curve following controller does does not localize, but uses the
+        xi = x.astype(int) # this *WILL FAIL*, but it will run.
+        height = V[xi[0], xi[1]]
+        gradient = np.array([VX[xi[0], xi[1]], VY[xi[0], xi[1]]])
+        ω = follow_level_curve(height, V_0, gradient)
+        
+        return ω
+    
     # run the animation
     for t in np.arange(0,duration,dt):
         # Reset matplotlib view
@@ -57,9 +75,10 @@ def run_car(duration, dt=.1):
         ax.set_ylim(ymin,ymax)
 
         # iterate the controler and draw vehicle
-        x = x + dt*straight_line(x)    
+        x = x + dt*flc(x)
         rl.draw_tank(x[[0,1,3]],'red',0.2) # x,y,θ
-        draw_field(animation_vars=(x, xmin, xmax, ymin, ymax))
+        draw_field(normalize=False
+                   , animation_vars=(x, xmin, xmax, ymin, ymax))
 
 if __name__ == "__main__":
     run_car(2)
--- potential_field_islands.py
+++ potential_field_islands.py
@@ -15,25 +15,44 @@ import matplotlib.pyplot as plt
 import roblib as rl
 import numpy as np
 
-def draw_field(normalize=True, three_d=False, animation_vars=None):
-    if animation_vars != None:
-        x, xmin, xmax, ymin, ymax = animation_vars
+def make_islands(xmin, xmax, ymin, ymax):
+    '''
+    Simply makes two 'islands' from gaussian distributions, joins them,
+    and returns data on the islands. Split off from draw_field, as the
+    data is needed by other functions. Could be better named.
+    
+    Args:
+      the bounds of the region
+    Returns:
+      a 5-tuple (X, Y, VX, VY, V)
+      X, Y:   arrow locations for plt.quiver
+      V: "potential" at every point; the height of the sea floor
+      VX, VY: The gradient in the X and Y direction, respectively, of V.
+    '''
     # x and y coordinates/locations of the arrows
     Mx    = np.arange(xmin,xmax,0.3)
     My    = np.arange(ymin,ymax,0.3)
-
+    
     # Draw islands
     X1, X2 = np.meshgrid(Mx,My)
     
     island_center1 = [xmin + (xmax - xmin) / 2, ymin + (ymax - ymin) / 2]
     island_center2 = [xmin + (xmax - xmin) * .2, ymin + (ymax - ymin) * .9]
     island_sum  = mnorm(island_center1, cov=2).pdf(np.dstack((X1, X2))) / 2
-    island_sum += mnorm(island_center2, cov=2).pdf(np.dstack((X1, X2))) 
-    
+    island_sum += mnorm(island_center2, cov=2).pdf(np.dstack((X1, X2)))
+    V = island_sum
 
     # VX, VY are x and y components
     VX = np.gradient(island_sum)[1]
     VY = np.gradient(island_sum)[0]
+
+    return (Mx, My, VX, VY, V)
+def draw_field(normalize=True, three_d=False, animation_vars=None):
+    if animation_vars != None:
+        x, xmin, xmax, ymin, ymax = animation_vars
+
+    Mx, My, VX, VY, V = make_islands(xmin, xmax, ymin, ymax)
+    X1, X2 = np.meshgrid(Mx,My)
     
     if(normalize):
         R=np.sqrt(VX**2+VY**2)
@@ -45,7 +64,7 @@ def draw_field(normalize=True, three_d=False, animation_vars=None):
         from mpl_toolkits.mplot3d import axes3d
         fig2 = plt.figure('0')
         ax1 = fig2.add_subplot(111, projection='3d')
-        ax1.plot_surface(X1, X2, island_sum)
+        ax1.plot_surface(X1, X2, V)
 
     return()
 
@@ -60,7 +79,7 @@ if __name__ == "__main__":
     dt   = 0.2
     fig  = plt.figure('Two normalized Island')
     ax   = fig.add_subplot(111, aspect='equal')
-    xmin,xmax,ymin,ymax=-5,5,-5,5
-
-    draw_field(three_d=True, normalize=False)
+    # xmin,xmax,ymin,ymax=-5,5,-5,5
+    av = (None,-5,5,-5,5)
+    draw_field(three_d=True, normalize=False, animation_vars=av)
     plt.show()
